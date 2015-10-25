d7e9dda21dcf77c3e57d337de3e5369420c98504
Dave Kinkead <dave@kinkead.com.au>
2015-10-25 16:52:28 +1000

    Add behaviour for edge cases

--- lib/the_oxford_comma.rb
+++ lib/the_oxford_comma.rb
@@ -2,18 +2,20 @@ require "the_oxford_comma/version"
 
 class Array
   def and
-    if size > 2
-      self[0..-2].join(", ") << ", and #{last}"
-    else
-      "#{first} and #{last}"
+    case size
+    when 0 then "" 
+    when 1 then first
+    when 2 then "#{first} and #{last}"
+    else self[0..-2].join(", ") << ", and #{last}"
     end
   end
 
   def or
-    if size > 2
-      self[0..-2].join(", ") << ", or #{last}"
-    else
-      "#{first} or #{last}"
+    case size
+    when 0 then "" 
+    when 1 then first
+    when 2 then "#{first} or #{last}"
+    else self[0..-2].join(", ") << ", or #{last}"
     end
   end
 end
--- test/oxford_spec.rb
+++ test/oxford_spec.rb
@@ -10,6 +10,14 @@ describe Array do
     it "conjoins pairs without commas" do
       ["bacon", "eggs"].and.must_equal "bacon and eggs"
     end
+
+    it "dosn't spit the dummy with single arrays" do
+      ["single"].and.must_equal "single"
+    end
+
+    it "fails silently with empty arrays" do
+      [].and.must_equal ""
+    end
   end
 
   describe "#or" do
@@ -19,6 +27,14 @@ describe Array do
 
     it "disjoins pairs without commas" do
       ["dead", "alive"].or.must_equal "dead or alive"
+    end
+
+    it "dosn't spit the dummy with single arrays" do
+      ["single"].or.must_equal "single"
+    end
+
+    it "fails silently with empty arrays" do
+      [].or.must_equal ""
     end
   end
 end
